ec83e4d939077be49dcef8a115f23f48e37632cb
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2022-04-02 22:33:25 +0900

    記事一覧を表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create({title: 'ももたろう', content: 'むかし、むかし、ある所におじいさんとおばあさんが住んでいました。'})
+Article.create({title: 'うらしまたろう', content: 'むかし、むかし、あるところに浦島太郎という心やさしい漁師が住んでいました。'})
