e8e9a75bcba575b9aa24a1e3b23f33e55347327b
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2022-04-13 10:45:14 +0900

    updateアクション

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,4 +9,25 @@ class ProfilesController < ApplicationController
   def edit
     @profile = current_user.build_profile
   end
+
+  def update
+    @profile = current_user.build_profile(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: '保存しました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.require(:profile).permit(
+      :nickname,
+      :introduction,
+      :gender,
+      :birthday,
+      :subscribed
+    )
+  end
 end
