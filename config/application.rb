47a0721cfff3f0974ad77045304fe2c2d9183985
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2022-04-19 10:52:14 +0900

    dotenvの導入

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Bundler.require(*Rails.groups)
+Dotenv::Railtie.load
+
 module BlogApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
