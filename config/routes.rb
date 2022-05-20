347850151208382b11b41269d42224eee618ec0c
Kazuki Ohta <kazuki.ohta.kazu@gmail.com>
2022-05-20 18:22:25 +0900

    ルーティングのグループ化

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,27 @@
 Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: 'articles#index'
+
+  resources :articles
 
   resource :timeline, only: [:show]
 
-  resources :articles do
-    resources :comments, only: [:index, :new, :create]
+  resource :profile, only: [:show, :edit, :update]
 
-    resource :like, only: [:show, :create, :destroy]
-  end
+  resources :favorites, only: [:index]
 
   resources :accounts, only: [:show] do
     resources :follows, only: [:create]
     resources :unfollows, only: [:create]
   end
 
-  resource :profile, only: [:show, :edit, :update]
-  resources :favorites, only: [:index]
+  namespace :api, defaults: {format: :json} do
+    scope '/articles/:article_id' do
+      resources :comments, only: [:index, :create]
+      resource :like, only: [:show, :create, :destroy]
+    end
+  end
 end
